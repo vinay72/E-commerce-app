f8af405d073718d63deed86253ba37d6c12074dc
Vinay <vinay@dono.consulting>
2021-01-15 03:40:02 +0530

    Made reusable SearchBox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
 
   }
@@ -17,13 +19,20 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <CardList name='Vinay'>
-          {
-            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e =>
+            this.setState({ searchField: e.target.value })
           }
-        </CardList>
+        />
+        <CardList monsters={filteredMonsters} />
 
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './card-list.styles.css'
+import React from "react";
+import "./card-list.styles.css";
+import { Card } from "../card/card.component"
 
 export const CardList = (props) => {
-    
-    return (
-        <div className="card-list">
-            {props.children}
-        </div>
-    )
-}
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <Card key={props.monsters.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
