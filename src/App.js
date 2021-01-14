47098c8ee7cff6aae752849cccf7e4eb5dde0ba3
Vinay <vinay@dono.consulting>
2021-01-15 00:23:22 +0530

    Organizing folder structure and adding CardList component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Vinay Sagar Sharma. Welcome to this Course.'
+      monsters: []
     }
 
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({
-            string: 'Hello Vikrant'
-          })}>Change the text</button>
-        </header>
+        <CardList name='Vinay'>
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
+
       </div>
     );
   }
