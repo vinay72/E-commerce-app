9466bd195e473ef2f643a0430537e8c562939ba4
Vinay <vinay@dono.consulting>
2021-01-15 23:31:28 +0530

    Added basic styling and completed this project

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
-
+    
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
   }
   render() {
     const { monsters, searchField } = this.state;
@@ -25,12 +29,10 @@ class App extends Component {
     )
     return (
       <div className="App">
-        
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e =>
-            this.setState({ searchField: e.target.value })
-          }
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,6 +3,7 @@ import "./card-list.styles.css";
 import { Card } from "../card/card.component"
 
 export const CardList = (props) => {
+  console.log('card list', props);
   return (
     <div className="card-list">
       {props.monsters.map((monster) => (
